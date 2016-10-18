4c35c41632d1d4171fab796c02d2e4c0054eb2b9
Eric Rosenberg <ehaydenr@gmail.com>
2016-10-17 19:03:16 -0500

    add daily script

---
+++ jobs/daily_script_air/daily_script_air.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import urllib2
+import time
+import os
+
+from bdeep.context import generateOutputPath
+
+new_folder_path = generateOutputPath(time.strftime("%d-%m-%Y"))
+new_file_name = generateOutputPath('air_data-' + time.strftime("%d-%m-%Y-%M") + '.csv')
+
+if not os.path.exists(new_folder_path):
+	os.makedirs(new_folder_path)
+os.system('mv ' + generateOutputPath('air_data.csv') + ' ' + new_folder_path)
+
+print "finished job2 for weekly"
